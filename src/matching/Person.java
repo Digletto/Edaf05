c199e498991042131df9e4802720fc7490e24e8b
dat15enu <ermann96@gmail.com>
2017-03-29 11:56:59 +0200

    push

---
+++ src/matching/Person.java
@@ -0,0 +1,30 @@
+package matching;
+
+import java.util.ArrayList;
+
+public class Person {
+
+	int engagementIndex= -1;
+
+	public int getEngagementIndex(){
+		
+		return engagementIndex;
+		
+	}
+
+	//SET INDEX TO INDEX OF RECIEVED PERSON IN PREFERENCE LIST
+	public void setEngagementIndex(Person m) {
+		
+		
+	}
+
+	public boolean prefers(Person m) {
+		return false;
+	}
+
+	public ArrayList<Person> getPreferenceList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
