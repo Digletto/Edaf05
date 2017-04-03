8b785690e02a94815cc1333b8159ac1c8b589503
Digletto <Digletto@live.com>
2017-04-03 16:00:18 +0200

    nothing important

--- src/usa/Road.java
+++ src/usa/Road.java
@@ -1,11 +1,12 @@
 package usa;
 
+//edge (or connection)
 public class Road {
 
 	public City other(City tempCity) {
 		// TODO Auto-generated method stub
 		return null;
-	} //edge (or connection)
+	} 
 
 	public int length() {
 		// TODO Auto-generated method stub
