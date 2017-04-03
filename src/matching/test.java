84fec61fbb201d512a4b0ff68cc4dc09816874c0
Digletto <Digletto@live.com>
2017-04-03 10:35:34 +0200

    merged

--- src/matching/test.java
+++ src/matching/test.java
@@ -8,7 +8,7 @@ public class test {
 
 	public static void main(String[] args) {
 //		Person p = new Person(2, "w");								//tests preflist parsing
-//		ParseFile pf = new ParseFile("tests/sm-kt-p-4.in.txt");
+		ParseFile pf = new ParseFile("tests/sm-kt-p-4.in.txt");
 //		ArrayList<Integer> prefList = new ArrayList<Integer>();
 //		prefList = pf.parsePref(p);
 //		for(Integer i : prefList)
