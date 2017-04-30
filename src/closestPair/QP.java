ecc86c358d9a2fa51e933095c92289598ef3c6e2
Digletto <Digletto@live.com>
2017-04-30 22:19:51 +0200

    formatting

--- src/closestPair/QP.java
+++ src/closestPair/QP.java
@@ -1,32 +1,32 @@
 package closestPair;
 
 public class QP {
-	
 
 	public static void main(String[] args) {
 		PointParser pp = new PointParser("test.txt");
 		XyList xy = pp.parse();
-		
+
 		run(xy);
 	}
 
 	private static Pair run(XyList xy) {
-		
+
 		if (xy.size() <= 3)
 			return xy.smallestDist();
-		
+
 		XyList left = xy.left();
 		XyList right = xy.right();
-		
+
 		Pair min = minOf(run(left), run(right));
-		
+
 		Line line = new Line(left.rightMostX());
 		XyList center = xyCenter(xy, line, min.dist());
 		Pair centerMin = center.next15();
-		
+
 		if (centerMin.dist() < min.dist())
 			return centerMin;
-		else return min;
+		else
+			return min;
 	}
 
 	private static XyList xyCenter(XyList xy, Line line, int dist) {
@@ -34,8 +34,9 @@ public class QP {
 	}
 
 	private static Pair minOf(Pair leftMin, Pair rightMin) {
-		if(leftMin.dist() < rightMin.dist())
+		if (leftMin.dist() < rightMin.dist())
 			return leftMin;
-		else return  rightMin;
+		else
+			return rightMin;
 	}
 }
