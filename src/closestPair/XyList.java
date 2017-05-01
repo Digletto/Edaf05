9af19d0c99f54af53d0884c42e97022823667267
enu <ermann96@gmail.com>
2017-05-01 14:49:28 +0200

    fixed left() and right() in XyList

--- src/closestPair/XyList.java
+++ src/closestPair/XyList.java
@@ -1,17 +1,55 @@
 package closestPair;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
 
 public class XyList {
-
+	
+	//Points sorted by x
+	private List<Point2D> xList;
+	//Points sorted by y
+	private List<Point2D> yList;
+	
+	
+	//Only in first recursive step
+	public XyList(ArrayList<Point2D> list){
+		
+		//TODO: SORT list into xList and yList
+		
+	}
+	
+	//Used in remaining recursive steps
+	public XyList(List<Point2D> xList, List<Point2D> yList){
+		
+		this.xList = xList;
+		this.yList = yList;
+		
+	}
+	
 	public XyList left() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Point2D> newXLx = new ArrayList<Point2D>();
+		List<Point2D> newXLy = new ArrayList<Point2D>();
+		
+		
+		//Make a new XyList consisting of first half of xList and yList
+		newXLx.addAll(xList.subList(0, xList.size()/2));
+		newXLy.addAll(xList.subList(0, yList.size()/2));
+		return new XyList(newXLx, newXLy);
 	}
 
 	public XyList right() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Point2D> newXLx = new ArrayList<Point2D>();
+		List<Point2D> newXLy = new ArrayList<Point2D>();
+		
+		
+		//Make a new XyList consisting of second half of xList and yList
+		newXLx.addAll(xList.subList(xList.size()/2, xList.size()));
+		newXLy.addAll(xList.subList(yList.size()/2, yList.size()));
+		return new XyList(newXLx, newXLy);
+		
 	}
 
 	public int rightMostX() {
@@ -31,7 +69,7 @@ public class XyList {
 
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return xList.size();
 	}
 
 	public Pair closestPair() {
