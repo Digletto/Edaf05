e8cf07a9093233385020a65638cfb267755d1465
Digletto <Digletto@live.com>
2017-05-02 00:19:34 +0200

    small fixes

--- src/closestPair/Pair.java
+++ src/closestPair/Pair.java
@@ -1,5 +1,4 @@
 package closestPair;
-
 import java.awt.geom.Point2D;
 
 public class Pair {
@@ -8,14 +7,11 @@ public class Pair {
 	private Point2D b;
 	
 	public Pair(Point2D a, Point2D b){
-		
 		this.a = a;
 		this.b = b;
-		
 	}
 	
 	public double dist() {
-		
 		return a.distance(b);	}
 
 }
--- src/closestPair/XyList.java
+++ src/closestPair/XyList.java
@@ -7,63 +7,57 @@ import java.util.Comparator;
 import java.util.List;
 
 public class XyList {
-	
-	//Points sorted by x
+
+	// Points sorted by x
 	private List<Point2D> xList;
-	//Points sorted by y
+	// Points sorted by y
 	private List<Point2D> yList;
-	
-	
-	//Only in first recursive step
-	public XyList(ArrayList<Point2D> list){
-		
+
+	// Only in first recursive step
+	public XyList(ArrayList<Point2D> list) {
+
 		Collections.sort(list, new xComparator());
 		xList = list;
 		Collections.sort(list, new yComparator());
 		yList = list;
-		
 	}
-	
-	//Used in remaining recursive steps
-	public XyList(List<Point2D> xList, List<Point2D> yList){
-		
+
+	// Used in remaining recursive steps
+	public XyList(List<Point2D> xList, List<Point2D> yList) {
+
 		this.xList = xList;
 		this.yList = yList;
-		
 	}
-	
+
 	public XyList left() {
-		
+
 		List<Point2D> newXLx = new ArrayList<Point2D>();
 		List<Point2D> newXLy = new ArrayList<Point2D>();
-		
-		
-		//Make a new XyList consisting of first half of xList and yList
-		newXLx.addAll(xList.subList(0, (xList.size()-1)/2));
-		newXLy.addAll(xList.subList(0, (yList.size()-1)/2));
+
+		// Make a new XyList consisting of first half of xList and yList
+		newXLx.addAll(xList.subList(0, (xList.size() - 1) / 2));
+		newXLy.addAll(xList.subList(0, (yList.size() - 1) / 2));
 		return new XyList(newXLx, newXLy);
 	}
 
 	public XyList right() {
-		
+
 		List<Point2D> newXLx = new ArrayList<Point2D>();
 		List<Point2D> newXLy = new ArrayList<Point2D>();
-		
-		
-		//Make a new XyList consisting of second half of xList and yList
-		newXLx.addAll(xList.subList((xList.size()-1)/2, xList.size()-1));
-		newXLy.addAll(xList.subList((yList.size()-1)/2, yList.size()-1));
+
+		// Make a new XyList consisting of second half of xList and yList
+		newXLx.addAll(xList.subList((xList.size() - 1) / 2, xList.size() - 1));
+		newXLy.addAll(xList.subList((yList.size() - 1) / 2, yList.size() - 1));
 		return new XyList(newXLx, newXLy);
-		
+
 	}
 
 	public double rightMostX() {
-		return xList.get(xList.size()-1).getX();
+		return xList.get(xList.size() - 1).getX();
 	}
 
-	public List<Pair> y() { // y ordered list
-		//TODO:
-		return null;
+	public List<Point2D> y() { // y ordered list
+		return yList;
 	}
 
 	public Pair next15() {
@@ -76,49 +70,40 @@ public class XyList {
 	}
 
 	public Pair closestPair() {
-		
-		if(xList.size() == 2){
-			
+
+		if (xList.size() == 2) {
 			return new Pair(xList.get(0), xList.get(1));
-			
-		}
-		else{
-			
+		} else {
 			Pair min = new Pair(xList.get(0), xList.get(1));
-			
-			for(Point2D p1 : xList){
-				
-				for(Point2D p2 : xList){
-				
-					if((new Pair(p1,p2)).dist() <min.dist())
-						min = new Pair(p1,p2);
-					
+			for (Point2D p1 : xList) {
+				for (Point2D p2 : xList) {
+					if ((new Pair(p1, p2)).dist() < min.dist())
+						min = new Pair(p1, p2);
 				}
-				
 			}
-			
 			return min;
-			
 		}
-		
-	}
-	
-	class xComparator implements Comparator<Point2D> {
-	    @Override
-	    public int compare(Point2D p1, Point2D p2) {
-	        if (p1.getX() < p2.getX()) return -1;
-	        if (p1.getX() > p2.getX()) return 1;
-	        return 0;
-	    }    
-	}
-	
-	class yComparator implements Comparator<Point2D> {
-	    @Override
-	    public int compare(Point2D p1, Point2D p2) {
-	        if (p1.getY() < p2.getY()) return -1;
-	        if (p1.getY() > p2.getY()) return 1;
-	        return 0;
-	    }    
 	}
 
+	class xComparator implements Comparator<Point2D> {
+		@Override
+		public int compare(Point2D p1, Point2D p2) {
+			if (p1.getX() < p2.getX())
+				return -1;
+			if (p1.getX() > p2.getX())
+				return 1;
+			return 0;
+		}
+	}
+
+	class yComparator implements Comparator<Point2D> {
+		@Override
+		public int compare(Point2D p1, Point2D p2) {
+			if (p1.getY() < p2.getY())
+				return -1;
+			if (p1.getY() > p2.getY())
+				return 1;
+			return 0;
+		}
+	}
 }
